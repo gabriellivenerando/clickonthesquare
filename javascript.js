33b94b5f58531fd0b3aedf08bc02e1d89aee434d
Gabrielli Venerando <gabieven14@gmail.com>
2020-04-22 23:31:33 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,161 @@
+const quadrado1 = document.querySelector('.div1')
+
+quadrado1.style.backgroundColor = 'white';
+
+function trocaDeCor1(){
+    if(quadrado1.style.backgroundColor == 'white'){
+        quadrado1.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado1.style.backgroundColor = 'white';
+    }
+
+}
+
+
+const quadrado2 = document.querySelector('.div2')
+
+quadrado2.style.backgroundColor = 'white';
+
+function trocaDeCor2(){
+    if(quadrado2.style.backgroundColor == 'white'){
+        quadrado2.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado2.style.backgroundColor = 'white';
+    }
+
+}
+
+
+const quadrado3 = document.querySelector('.div3')
+
+quadrado3.style.backgroundColor = 'white';
+
+function trocaDeCor3(){
+    if(quadrado3.style.backgroundColor == 'white'){
+        quadrado3.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado3.style.backgroundColor = 'white';
+    }
+
+}
+
+const quadrado4 = document.querySelector('.div4')
+
+quadrado4.style.backgroundColor = 'white';
+
+function trocaDeCor4(){
+    if(quadrado4.style.backgroundColor == 'white'){
+        quadrado4.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado4.style.backgroundColor = 'white';
+    }
+
+}
+
+const quadrado5 = document.querySelector('.div5')
+
+quadrado5.style.backgroundColor = 'white';
+
+function trocaDeCor5(){
+    if(quadrado5.style.backgroundColor == 'white'){
+        quadrado5.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado5.style.backgroundColor = 'white';
+    }
+
+}
+
+const quadrado6 = document.querySelector('.div6')
+
+quadrado6.style.backgroundColor = 'white';
+
+function trocaDeCor6(){
+    if(quadrado6.style.backgroundColor == 'white'){
+        quadrado6.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado6.style.backgroundColor = 'white';
+    }
+
+}
+
+
+const quadrado7 = document.querySelector('.div7')
+
+quadrado7.style.backgroundColor = 'white';
+
+function trocaDeCor7(){
+    if(quadrado7.style.backgroundColor == 'white'){
+        quadrado7.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado7.style.backgroundColor = 'white';
+    }
+
+}
+
+
+const quadrado8 = document.querySelector('.div8')
+
+quadrado8.style.backgroundColor = 'white';
+
+function trocaDeCor8(){
+    if(quadrado8.style.backgroundColor == 'white'){
+        quadrado8.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado8.style.backgroundColor = 'white';
+    }
+
+}
+
+const quadrado9 = document.querySelector('.div9')
+
+quadrado9.style.backgroundColor = 'white';
+
+function trocaDeCor9(){
+    if(quadrado9.style.backgroundColor == 'white'){
+        quadrado9.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado9.style.backgroundColor = 'white';
+    }
+
+}
+
+const quadrado10 = document.querySelector('.div10')
+
+quadrado10.style.backgroundColor = 'white';
+
+function trocaDeCor10(){
+    if(quadrado10.style.backgroundColor == 'white'){
+        quadrado10.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado10.style.backgroundColor = 'white';
+    }
+
+}
+
+
+const quadrado11 = document.querySelector('.div11')
+
+quadrado11.style.backgroundColor = 'white';
+
+function trocaDeCor11(){
+    if(quadrado11.style.backgroundColor == 'white'){
+        quadrado11.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado11.style.backgroundColor = 'white';
+    }
+
+}
+
+
+const quadrado12 = document.querySelector('.div12')
+
+quadrado12.style.backgroundColor = 'white';
+
+function trocaDeCor12(){
+    if(quadrado12.style.backgroundColor == 'white'){
+        quadrado12.style.backgroundColor = 'darksalmon';
+    }else{
+        quadrado12.style.backgroundColor = 'white';
+    }
+
+}
